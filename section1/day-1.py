32708d2a9ad32beaadd23a1b4a9a2249fb78f3bc
Cristian Cepeda <camilo2017@gmail.com>
2024-02-29 00:02:53 -0600

    Day 1 of 100

---
+++ section1/day-1.py
@@ -0,0 +1,48 @@
+# name = input("What is your Name? ")
+# print("Hello " + name + "!")
+#
+# print("Hello world!")
+
+# # Video 8
+# print('1. Max 500g ...')
+# print('2. Knead the dough ...')
+# print('She said: "hello" and then left')
+
+# # Video 9
+# print('HelloWorld\nHelloWorld\nHellowWorld')
+# print('Hello'+"Angela")
+
+# # Video 11
+# # NOTES : Input runs first and collects the value the user enters and then
+# # passes it into the print method.
+# print("Hello " + input('What is your Name? >  ') + "!")
+
+# Video 12
+# print(input())
+# print(input())
+
+# num1 = int(input())
+# num2 = int(input())
+# print(num1 * num2)
+
+# print(len(input("What is your Name ")))
+
+# # Video 13
+# name = input("What is your Name")
+# print(name)
+# name = "jack"
+# print(name)
+
+# # Video 14
+# a = input()
+# b = input()
+# print("a: " + b)
+# print("b: " + a)
+
+# Video 16
+print("Hello to first project!\n-----------------------------")
+grow_up_city = input("What city did you grow up in? \n> ")
+favorite_pet_name = input("What is your favorite pet's name? \n> ")
+print("Your band name could be " + grow_up_city + " " + favorite_pet_name)
+
+
